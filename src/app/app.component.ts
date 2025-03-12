986fdbbb4508c51299bc82dd6c21aadf9a79621a
Aditya Kalambe <adityakalambe20@gmail.com>
2025-03-12 21:55:28 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CameraComponent } from './camera/camera.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CameraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/camera.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CameraService {
+
+  constructor() { }
+  getCameraStream(): Promise<MediaStream> {
+    return new Promise((resolve, reject) => {
+      // Request camera access
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          resolve(stream);
+        })
+        .catch((err) => {
+          console.error('Error accessing camera: ', err);
+          reject(err);
+        });
+    });
+  }
+
+  stopCameraStream(stream: MediaStream): void {
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => track.stop());
+  }
+}
